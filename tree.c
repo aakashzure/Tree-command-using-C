a02351ed61876d8deb416a09b7318b113d3b0b67
aakashzure <aakashzure@gmail.com>
2016-11-07 11:17:30 +0530

    Update tree.c

--- tree.c
+++ tree.c
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (C) Zure Aakash Vasantrao 
+ * Copyright (C) Zure Aakash Vasantrao zureav16.comp@coep.ac.in
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
