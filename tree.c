65ccb64c25d9e2385a558a9340624a2aa64fa4c1
aakashzure <aakashzure@gmail.com>
2016-11-07 11:12:55 +0530

    Add files via upload
    problems with option -i and -f fixed

--- tree.c
+++ tree.c
@@ -23,40 +23,47 @@
 #include <dirent.h>
 #include <errno.h>
 #include <stdio.h>
-int a=-1;
+int a = -1;
+int fcount = 0, dcount = 0;
 void ifiles(const char *name)
 {
 	DIR *dir;
 	char * occ,  space[128], line[128], buf[128];
 	int i;
 	struct dirent *filename;
-	strcpy(space,"");
-	strcpy(buf, "");
-	strcpy(line,"|-----");
 	if (!(dir = opendir(name))){
 	printf("tree : '%s' : %s\n", name,strerror(errno));
-	return;
-	}
+        return;
+        }
 	if (!(filename = readdir(dir))){
-	printf("%s\n", strerror(errno));
-	return;
+		printf("%s\n", strerror(errno));
+		return;
 	}
-	for(i=0;i<=a;i++)
-	strcat(space, "|\t");
+	if(a >= 0){
+		occ = strrchr(name,'/');
+		i = occ - name;
+		strncpy(buf, name+i+1, strlen(name));	
+	}
+	printf("%s\n", buf);
 	do {
 		if (filename->d_type == DT_DIR) {
 			char path[1024];
-			snprintf(path, sizeof(path), "%s", filename->d_name);
+			snprintf(path, sizeof(path), "%s/%s", name, filename->d_name);
 			if (strcmp(filename->d_name, ".") == 0 || strcmp(filename->d_name, "..") == 0)
 				continue;
+			a++;
+			dcount++;
 			ifiles(path);
-			}
-		else {
-			printf("%s\n",filename->d_name);
 		}
-	}while ((filename = readdir(dir)));
+		else{
+			printf("%s\n",filename->d_name);
+			a = -1;
+			fcount++;
+		}
+	} while ((filename = readdir(dir)));
 	closedir(dir);
 }
+
 void files(const char *name)
 {
 	DIR *dir;
@@ -89,11 +96,13 @@ void files(const char *name)
 			if (strcmp(filename->d_name, ".") == 0 || strcmp(filename->d_name, "..") == 0)
 				continue;
 			a++;
+			dcount++;
 			files(path);
 		}
 		else{
 			printf("%s%s%s\n",space,line,filename->d_name);
 			a = -1;
+			fcount++;
 		}
 	} while ((filename = readdir(dir)));
 	closedir(dir);
@@ -130,10 +139,12 @@ void folders(const char *name)
 				continue;
 			a++;
 			printf("%s%s%s\n",space,line,filename->d_name);
+			dcount++;
 			folders(path);
 		}
 		else{             
 			a = -1;
+			fcount++;
 		}
 	}while ((filename = readdir(dir)));
 	closedir(dir);
@@ -180,11 +191,13 @@ void ffiles(const char *name)
 			if (strcmp(filename->d_name, ".") == 0 || strcmp(filename->d_name, "..") == 0)
 				continue;
 			a++;
+			dcount++;
 			ffiles(path);
 		}
 		else{
 			printf("%s%s%s/%s\n",space,line,name,filename->d_name);
 			a = -1;
+			fcount++;
 		}
 	} while ((filename = readdir(dir)));
 	closedir(dir);
@@ -199,27 +212,34 @@ void fifiles(const char *name)
 	strcpy(buf, "");
 	strcpy(line,"|-----");
 	if (!(dir = opendir(name))){
-		printf("tree : '%s' : %s\n", name,strerror(errno));
-		return;
-	}
+	printf("tree : '%s' : %s\n", name,strerror(errno));
+        return;
+        }
 	if (!(filename = readdir(dir))){
 		printf("%s\n", strerror(errno));
 		return;
 	}
-	for(i=0;i<=a;i++)
-		strcat(space, "|\t");
+	/*for(i=0;i<=a;i++)
+	strcat(space, "|\t");
+	if(a >= 0){
+		occ = strrchr(name,'/');
+		i = occ - name;*/
+	strncpy(buf, name+1, strlen(name));	
+	printf("%s\n", buf);
 	do {
 		if (filename->d_type == DT_DIR) {
 			char path[1024];
 			snprintf(path, sizeof(path), "%s/%s", name, filename->d_name);
 			if (strcmp(filename->d_name, ".") == 0 || strcmp(filename->d_name, "..") == 0)
-                		continue;
+				continue;
 			a++;
+			dcount++;
 			fifiles(path);
 		}
 		else{
-			printf("%s/%s\n",name, filename->d_name);
+			printf("%s/%s\n",name,filename->d_name);
 			a = -1;
+			fcount++;
 		}
 	} while ((filename = readdir(dir)));
 	closedir(dir);
@@ -228,10 +248,12 @@ int main(int argc, char *argv[]){
 	if(argc == 2) {
 		if(argv[1] == NULL){ 
 			files(".");
+			printf("%d files and %d folders\n", fcount, dcount);
 			return 0;
 		}
 		if(strcmp(argv[1],".") == 0){
 			files(".");
+			printf("%d files and %d folders\n", fcount, dcount);
 			return 0;
 		}
 		if(strcmp(argv[1],"-h") == 0){
@@ -240,35 +262,44 @@ int main(int argc, char *argv[]){
 		}
 		else {
 			files(argv[1]);
+			printf("%d files and %d folders\n", fcount, dcount);
+			return 0;
 		}		
 	}
 	else if(argc == 3) {
 		
 		if(strcmp(argv[2],"-d") == 0){
 			folders(argv[1]);
+			printf("%d folders\n", dcount);
 			return 0;
 		}
 		if(strcmp(argv[2],"-f") == 0){
 			ffiles(argv[1]);
+			printf("%d files and %d folders\n", fcount, dcount);
 			return 0;
 		}
 		if(strcmp(argv[2],"-i") == 0){
 			ifiles(argv[1]);
+			printf("%d files and %d folders\n", fcount, dcount);
 			return 0;
 		}
 	}
 	else {
 		if((strcmp(argv[2],"-f") && strcmp(argv[3],"-i")) == 0){
 			fifiles(argv[1]);
+			printf("%d files and %d folders\n", fcount, dcount);
 			return 0;
 		}
 		if((strcmp(argv[2],"-i") && strcmp(argv[3],"-f")) == 0){
 			fifiles(argv[1]);
+			printf("%d files and %d folders\n", fcount, dcount);
 			return 0;
 		}
 		else {
 			files(".");
+			printf("%d files and %d folders\n", fcount, dcount);
+			return 0;
 		}
 	}
-	
+	return 0;
 }
